a70dd6ad27d5a24e1b91c2d95df9d9095ce6a4ef
zacharie naim <melvin.naim@gmail.com>
2025-03-30 14:49:05 +0300

    Building a icons to GameCard

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, Image } from "@chakra-ui/react";
 import { Game } from "../hook/useGames";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
   game: Game;
@@ -7,10 +8,18 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root overflow={"hidden"} borderRadius="10px">
+    <Card.Root
+      overflow={"hidden"}
+      borderRadius="10px"
+      bg="gray.800"
+      borderColor="white"
+    >
       <Image src={game.background_image} />
       <Card.Body>
         <Card.Title>{game.name}</Card.Title>
+        <PlatformIconsList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </Card.Body>
     </Card.Root>
   );
